ca4e169ecac9d7648a8b2e9a0a28f5c44c523654
Omar Bittar <Omar.Bittar@bitrate-it.com>
2021-12-28 17:48:01 +0000

    fix

--- config/server.js
+++ config/server.js
@@ -4,11 +4,7 @@ module.exports = ({ env }) => ({
   url: env('URL', 'http://localhost:1337'),
   admin: {
     auth: {
-<<<<<<< HEAD
       secret: env("JWT_SECRET")
-=======
-      secret: env('JWT_SECRET')
->>>>>>> c907128c4266b2ce5990098f337b0217cd803fcc
     }
   }
 });
